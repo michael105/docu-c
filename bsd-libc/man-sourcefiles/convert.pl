f84ccbc56c01337cbe27672e88ed5ae1a477e15a
Michael Myer <misc.myer@zoho.com>
2019-03-25 16:56:14 +0100

    minor changes

--- bsd-libc/man-sourcefiles/convert.pl
+++ bsd-libc/man-sourcefiles/convert.pl
@@ -1,8 +1,45 @@
 #!/bin/perl -w
 #
 
+my @F;
+my $fp;
+
+sub grab{
+		my $t = shift;
+		my @a;
+		while ( !( $F[$fp] =~ s/^\.$t (.*)\n/$1/ ) && ( $F[$fp++] ) ) {};
+		do {
+				push @a,$F[$fp]
+		} while (( $F[$fp++] ) && ( $F[$fp] =~ s/^\.$t (.*).\n/$1/ ) );
+
+		return( @a );
+}
 
 
+sub parse{
 
+	 print "-", grab( "Nm" ), " := ", grab ("Nd");
+	 # library = "Lb"
+	 # files = "In"
+	 grab( "!^Ft" );
+	 while ( $F[$fp] =~ s/^.Ft (.*)\n/$1/ ){
+			 print $F[$fp];
+			 $fp ++;
+			 $F[$fp] =~ /^.Fn (.*)\n/;
+			 print $1;
+			 $fp ++;
+	 }
+
+
+}
+	
+
+foreach my $fn ( @ARGV ){
+		open $fh, "<", $fn or die;
+		@F = <$fh>;
+		close $fh;
+		$fp = 0;
+		parse( );
+}
 
 
